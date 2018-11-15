9e0bb5ded37d1617b5bed76742946d3faea4f612
Lalit Mohan <lalit.mohan@pb.com>
2018-11-15 23:03:45 +0530

    sfsf

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by la024mo on 11/15/2018.
+ */
+public class HelloWorld {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World to Everyone :)");
+    }
+
+}
+
